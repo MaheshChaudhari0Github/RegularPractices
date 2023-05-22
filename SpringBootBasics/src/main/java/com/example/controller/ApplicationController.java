882561aad01634b8353a594e7e13fb3fb0d616fb
rajesh221133 <115137710+rajesh221133@users.noreply.github.com>
2023-05-22 16:28:06 +0530

    Regular

---
+++ SpringBootBasics/src/main/java/com/example/controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+	@GetMapping
+	public String sayHello() {
+		return "Welcome to Anudip Foundation";
+	}
+
+	@GetMapping("/services")
+	public String sayServices() {
+		return "SD";
+
+	}
+
+}
